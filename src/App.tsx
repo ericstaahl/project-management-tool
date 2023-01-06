a8de5978f3ba784cacff33f8ff6eb666a304ff1c
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2023-01-06 15:01:50 +0100

    Added css file for some global styling.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import HomePage from './pages/HomePage';
 import React from 'react';
+import './assets/css/style.css';
 
 const App: React.FC = () => {
   return (
