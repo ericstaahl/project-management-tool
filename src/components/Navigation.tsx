d82fff2219ab2e683725f97eaa36524a06ac2fee
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2023-01-06 15:02:23 +0100

    Styled navigation and HomePage/dashboard. Added some mock data.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,20 +1,40 @@
 import styled from '@emotion/styled';
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavContainer = styled.div({
+  display: 'flex',
+  columnGap: '1.5rem',
+  fontSize: '0.8rem',
+  alignItems: 'center',
+  paddingLeft: '1rem',
+});
 
 const Nav = styled.nav({
   display: 'flex',
+  columnGap: '1rem',
+  fontSize: '1.2rem',
+});
+
+const StyledLink = styled(Link)({
+  color: '#f5f5f5',
   textDecoration: 'none',
-  rowGap: '1rem',
+  ':hover': {
+    textDecoration: 'underline',
+  },
 });
 
 const Navigation: React.FC = () => {
   return (
     <div>
-      <Nav>
-        <a href='/'>Dashboard</a>
-        <a href='#'>Projects</a>
-        <a href='#'>About</a>
-      </Nav>
+      <NavContainer>
+        <h1>Project Mangement Tool</h1>
+        <Nav>
+          <StyledLink to='/'>Dashboard</StyledLink>
+          <StyledLink to='#'>Projects</StyledLink>
+          <StyledLink to='#'>About</StyledLink>
+        </Nav>
+      </NavContainer>
     </div>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,13 +3,64 @@ import React from 'react';
 
 const Container = styled.div({
   borderRadius: '5px',
+  backgroundColor: '#1c1c1c',
+  margin: '1rem',
+  padding: '2rem',
 });
+
+const GridContainer = styled.div({
+  display: 'grid',
+  padding: '1rem',
+  gridTemplateColumns: '1fr 1fr 1fr 1fr',
+  gridColumn: '1 / 1',
+  columnGap: '1rem',
+  rowGap: '1rem',
+});
+
+const Project = styled.div({
+  borderRadius: '5px',
+  minHeight: '200px',
+  backgroundColor: '#363942',
+  padding: '1rem',
+});
+
+const projects = [
+  {
+    title: 'project1',
+    id: 1,
+    number_of_members: 2,
+  },
+  {
+    title: 'project2',
+    id: 2,
+    number_of_members: 4,
+  },
+  {
+    title: 'project3',
+    id: 3,
+    number_of_members: 5,
+  },
+  {
+    title: 'project4',
+    id: 4,
+    number_of_members: 5,
+  },
+];
 
 const HomePage: React.FC = () => {
   return (
     <div>
       <Container>
-        <h1>HomePage</h1>
+        <h1>Dashboard</h1>
+        <GridContainer>
+          {projects?.map((project) => (
+            <Project key={project.id}>
+              <h3>{project.title}</h3>
+              <div>{`Number of members: ${project.number_of_members}`}</div>
+            </Project>
+          ))}
+          <Project></Project>
+        </GridContainer>
       </Container>
     </div>
   );
