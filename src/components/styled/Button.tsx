537a0bb1fd5b53e4581f02d9cbd3fa6f5f74fa71
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2023-02-18 14:15:26 +0100

    Changed button styling.

--- src/components/styled/Button.tsx
+++ src/components/styled/Button.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const StyledButton = styled.button({
+    fontSize: '0.9rem',
     alignSelf: 'start',
     backgroundColor: '#1927c2',
-    padding: '0.3rem 0.5rem 0.3rem 0.5rem',
+    padding: '0.5rem 1rem',
     border: '0.1rem solid #1927c2',
     borderRadius: '0.2rem',
     minWidth: '65px',
