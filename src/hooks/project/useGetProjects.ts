f49aaa9a8356da6fb156c239ac39204efcf478eb
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2023-02-18 11:50:14 +0100

    Updated axios.

--- src/hooks/project/useGetProjects.ts
+++ src/hooks/project/useGetProjects.ts
@@ -6,42 +6,48 @@ import useAuth from '../../context/AuthContext';
 
 const API_URL: string = import.meta.env.VITE_API_URL;
 
-const useGetProjects = (): {
-  data:
-    | Array<{
-        title: string;
-        project_id: number;
-        number_of_members: number;
-        start_date: string;
-        due_date: string;
-        _count: {
-          todo: number;
-        };
-      }>
-    | undefined;
-  isLoading: boolean;
+const useGetProjects = (
+    sortBy: string
+): {
+    data:
+        | Array<{
+              title: string;
+              project_id: number;
+              number_of_members: number;
+              start_date: string;
+              due_date: string;
+              _count: {
+                  todo: number;
+              };
+          }>
+        | undefined;
+    isLoading: boolean;
 } => {
-  const auth = useAuth();
-  console.log(auth);
-  const { data, isLoading } = useQuery({
-    queryKey: projectQueryKeys.lists(),
-    queryFn: async (): Promise<Projects> => {
-      const res = await axios.get<Projects>(`${API_URL}/projects`, {
-        headers: {
-          Authorization:
-            auth !== null && auth !== undefined
-              ? `Bearer ${auth.access_token}`
-              : '',
+    const auth = useAuth();
+    console.log(auth);
+    console.log('sortBy:', sortBy);
+    const { data, isLoading } = useQuery({
+        queryKey: projectQueryKeys.lists(sortBy),
+        queryFn: async (): Promise<Projects> => {
+            const res = await axios.get<Projects>(`${API_URL}/projects`, {
+                headers: {
+                    Authorization:
+                        auth !== null && auth !== undefined
+                            ? `Bearer ${auth.access_token}`
+                            : '',
+                },
+                params: {
+                    sortRule: sortBy,
+                },
+            });
+            if (res.status === 200) {
+                return res.data;
+            } else throw new Error('An error occured');
         },
-      });
-      if (res.status === 200) {
-        return res.data;
-      } else throw new Error('An error occured');
-    },
-    enabled: auth !== null,
-  });
+        enabled: auth !== null,
+    });
 
-  return { data, isLoading };
+    return { data, isLoading };
 };
 
 export default useGetProjects;
