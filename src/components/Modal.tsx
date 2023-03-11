03ffe042d1cc158ef409e4925b27f928ab47ef5d
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2023-03-11 14:25:41 +0100

    Refactored code.

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -15,25 +15,34 @@ const CloseModal = styled.div({
     cursor: 'pointer',
 });
 
+const ModalContainer = styled.div({
+    backgroundColor: 'rgb(0 0 0 / 80%)',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    zIndex: 999,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    width: '100vw',
+});
+
+const ModalWrapper = styled.div({
+    height: '80vh',
+    width: '80vw',
+    borderRadius: '0.5rem',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+});
+
 const Modal: React.FC<Props> = ({ handleSetShowModal, children }) => {
     return (
         <>
             {createPortal(
-                <div
-                    style={{
-                        backgroundColor: 'rgb(0 0 0 / 80%)',
-                        position: 'fixed',
-                        top: 0,
-                        left: 0,
-                        zIndex: 999,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        height: '100vh',
-                        width: '100vw',
-                    }}
-                >
+                <ModalContainer>
                     <CloseModal
                         onClick={() => {
                             handleSetShowModal();
@@ -41,19 +50,8 @@ const Modal: React.FC<Props> = ({ handleSetShowModal, children }) => {
                     >
                         x
                     </CloseModal>
-                    <div
-                        style={{
-                            height: '80vh',
-                            width: '80vw',
-                            borderRadius: '0.5rem',
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}
-                    >
-                        {children ?? <></>}
-                    </div>
-                </div>,
+                    <ModalWrapper>{children ?? <></>}</ModalWrapper>
+                </ModalContainer>,
                 document.body
             )}
         </>
