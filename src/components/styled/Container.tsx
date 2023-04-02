3f7985f8ea369102b1ad9d86f9745ffdab7d3bda
ericstaahl <94179820+ericstaahl@users.noreply.github.com>
2023-04-02 12:05:48 +0200

    Removed background color of Container.

--- src/components/styled/Container.tsx
+++ src/components/styled/Container.tsx
@@ -3,11 +3,8 @@ import styled from '@emotion/styled';
 import { breakpoints } from '../../lib/breakpoints';
 
 const StyledContainer = styled.div`
-    border-radius: 5px;
-    background-color: #1c1c1c;
     margin: 0.5rem;
     padding: 1rem;
-    box-shadow: 0 0 0.2rem #111;
     @media (min-width: ${breakpoints.md}) {
         margin: 1rem;
         padding: 2rem;
